9a50b624c836efcf5771880f6b1e322de07fc7a7
Dino Manco <mancosanto@gmail.com>
2022-04-04 11:50:58 +0200

    upgraded to react 18

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+const AppWithCallbackAfterRender = () => {
+    useEffect(() => {
+        console.log('rendered');
+    });
+
+    return <App />;
+};
+root.render(<AppWithCallbackAfterRender />);
